200b1e6008c6399afc42027d2f352562e3cc76e2
Pavelius <p000000000001@gmail.com>
2026-09-22 08:04:32 +0300

    Event conceptions.

--- ose/event.cpp
+++ ose/event.cpp
@@ -1,11 +1,5 @@
-#include "creature.h"
-#include "game.h"
 #include "event.h"
 
 int eventi::index() const {
 	return this - events;
 }
-
-eventi events[] = {
-	{ImageCaveEntrance, PlayerCharged, 0, Strenght, -4},
-};
---
+++ ose/game_events.cpp
@@ -0,0 +1,7 @@
+#include "creature.h"
+#include "event.h"
+#include "game.h"
+
+eventi events[] = {
+	{ImageCaveEntrance, PlayerCharged, 0, Strenght, -4},
+};
